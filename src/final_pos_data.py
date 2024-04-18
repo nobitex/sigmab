efdd91fdb30a6a0ecc1d8957671c004881520e6b
toolabi <prds.toolabi@gmail.com>
2024-04-18 21:10:19 +0330

    Feature(circuit): derinig data from test json file.

--- src/final_pos_data.py
+++ src/final_pos_data.py
@@ -1,26 +1,43 @@
+from utils.utils import get_solvency_data
+from utils.utils import combine_proof_files
 from utils.ecdsa_proof import generate_signature_data
 from utils.ecdsa_proof import generate_signature_proof_verification_data
 from utils.mpt_proof import generate_mpt_proof_data
 from utils.sba_proof import generate_sba_proof_data
 from utils.pol_proof import generate_pol_proof_data
+import time
+import random
+import os
+import sys
+import io, json
 
+# proof_of_liability_count = 360,000
+# Generate a random integer between 0 and 10^5
+salt = random.randint(0, 10**5)
 
-salt = 123
-message = "Nobitex"
-address_list = ["0x727d270cB6d427A431b0C5A88AD6491712c86061", "0x27A0cfeE639c8cd775FdD4E34210f148dDB041A1"] 
-num_sks = len(address_list)
+#starting time
+start = time.time()
+
+# get data
+message, signature_data, address_array, num_accounts, balances = get_solvency_data()
 
 # ecdsa_proof_data
-signature_data = generate_signature_data(num_sks)
 generate_signature_proof_verification_data(signature_data, message, salt)
 
 # mpt_proof_data
-generate_mpt_proof_data(address_list)
+generate_mpt_proof_data(address_array, salt)
 
 # sba_proof_data
-
-balances = [90313886187000, 518994566333498]
-generate_sba_proof_data(balances)
+generate_sba_proof_data(balances, salt)
 
 # pol_proof_data
-generate_pol_proof_data()
+generate_pol_proof_data(salt)
+
+# generate one single json file for all generated proofs
+combine_proof_files()
+
+# end time
+end = time.time()
+
+# total time taken
+print("Execution time of the program is- ", end-start)
