27ec948b2da71f4c73be6127e3d4dc72341e315f
MohammadAli Heydari <ostadgeorge@gmail.com>
2024-05-06 16:56:46 +0330

    add mpt las cicuit impl

--- src/circuits/mpt_last.py
+++ src/circuits/mpt_last.py
@@ -1,4 +1,5 @@
 from circuits import AbstractCircuit
+import rlp
 
 
 class MPTLastCircuit(AbstractCircuit):
@@ -6,7 +7,28 @@ class MPTLastCircuit(AbstractCircuit):
     MAX_LOWER_LENGTH = 99
     MAX_PREFIX_LENGTH = MAX_BLOCKS * 136 - MAX_LOWER_LENGTH
 
-    def generate_witness(
+    def generate_witness(self, exchange_account, block, ecdsa_commitment, salt):
+        proof = exchange_account.get_value("account_proof")
+        rev_proof = proof.accountProof[::-1]
+        layers = []
+        account_rlp = rlp.encode(
+            [proof.nonce, proof.balance, proof.storageHash, proof.codeHash]
+        )
+        address_bytes = bytes.fromhex(str(proof.address)[2:])
+        prefix_account_rlp = proof.accountProof[-1][: -len(account_rlp)]
+
+        return self._generate_witness(
+            salt,
+            address_bytes,
+            bytes(prefix_account_rlp),
+            proof.nonce,
+            proof.balance,
+            proof.storageHash,
+            proof.codeHash,
+            ecdsa_commitment,
+        )
+
+    def _generate_witness(
         self,
         salt,
         addressBytes,
