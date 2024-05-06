5b007945d984149c501b08f00026f297bcfcb7e9
MohammadAli Heydari <ostadgeorge@gmail.com>
2024-05-06 16:56:54 +0330

    add mpt path cicuit impl

--- src/circuits/mpt_path.py
+++ src/circuits/mpt_path.py
@@ -7,14 +7,14 @@ class MPTPathCircuit(AbstractCircuit):
     def generate_witness(self, salt, lower, upper, is_top):
         numLowerLayerBytes = len(lower)
         numUpperLayerBytes = len(upper)
-        lowerLayer = list(lower) + (MAX_BLOCKS * 136 - len(lower)) * [0]
-        upperLayer = list(upper) + (MAX_BLOCKS * 136 - len(upper)) * [0]
+        lowerLayer = list(lower) + (self.MAX_BLOCKS * 136 - len(lower)) * [0]
+        upperLayer = list(upper) + (self.MAX_BLOCKS * 136 - len(upper)) * [0]
 
         return super().generate_witness(
             salt=str(salt),
             numLowerLayerBytes=numLowerLayerBytes,
             numUpperLayerBytes=1 if is_top else numUpperLayerBytes,
             lowerLayerBytes=lowerLayer,
-            upperLayerBytes=[0] * MAX_BLOCKS * 136 if is_top else upperLayer,
+            upperLayerBytes=[0] * self.MAX_BLOCKS * 136 if is_top else upperLayer,
             isTop=1 if is_top else 0,
         )
