cac249fb625b3b2cac65b33b086a51c128e64a28
toolabi <prds.toolabi@gmail.com>
2024-05-21 16:42:00 +0330

    Feature(scripts): making some minor modfications.

--- src/config.py
+++ src/config.py
@@ -8,51 +8,51 @@ PROVIDER = os.environ.get(
 
 MPT_LAST_WITNESS_GEN_PATH = os.environ.get(
     "MPT_LAST_WITNESS_GEN_PATH",
-    "sigmab/circuit/temp/mpt_last/mpt_last_cpp/mpt_last",
+    "circuit/temp/mpt_last/mpt_last_cpp/mpt_last",
 )
 MPT_LAST_ZK_PARAMS_PATH = os.environ.get(
     "MPT_LAST_ZK_PARAMS_PATH",
-    "sigmab/circuit/temp/mpt_last/mpt_last_0001.zkey",
+    "circuit/temp/mpt_last/mpt_last_0001.zkey",
 )
 
 MPT_PATH_WITNESS_GEN_PATH = os.environ.get(
     "MPT_PATH_WITNESS_GEN_PATH",
-    "sigmab/circuit/temp/mpt_path/mpt_path_cpp/mpt_path",
+    "circuit/temp/mpt_path/mpt_path_cpp/mpt_path",
 )
 MPT_PATH_ZK_PARAMS_PATH = os.environ.get(
     "MPT_PATH_ZK_PARAMS_PATH",
-    "sigmab/circuit/temp/mpt_path/mpt_path_0001.zkey",
+    "circuit/temp/mpt_path/mpt_path_0001.zkey",
 )
 
 ECDSA_WITNESS_GEN_PATH = os.environ.get(
     "ECDSA_WITNESS_GEN_PATH",
-    "sigmab/circuit/temp/ecdsa_verify/ecdsa_verify_cpp/ecdsa_verify",
+    "circuit/temp/ecdsa_verify/ecdsa_verify_cpp/ecdsa_verify",
 )
 ECDSA_ZK_PARAMS_PATH = os.environ.get(
     "ECDSA_ZK_PARAMS_PATH",
-    "sigmab/circuit/temp/ecdsa_verify/ecdsa_verify_0001.zkey",
+    "circuit/temp/ecdsa_verify/ecdsa_verify_0001.zkey",
 )
 
 SBA_WITNESS_GEN_PATH = os.environ.get(
     "SBA_WITNESS_GEN_PATH",
-    "sigmab/circuit/temp/stealth_balance_addition/stealth_balance_addition_cpp/stealth_balance_addition",
+    "circuit/temp/stealth_balance_addition/stealth_balance_addition_cpp/stealth_balance_addition",
 )
 SBA_ZK_PARAMS_PATH = os.environ.get(
     "SBA_ZK_PARAMS_PATH",
-    "sigmab/circuit/temp/stealth_balance_addition/stealth_balance_addition_0001.zkey",
+    "circuit/temp/stealth_balance_addition/stealth_balance_addition_0001.zkey",
 )
 
 POL_WITNESS_GEN_PATH = os.environ.get(
     "POL_WITNESS_GEN_PATH",
-    "sigmab/circuit/temp/pol/pol_cpp/pol",
+    "circuit/temp/pol/pol_cpp/pol",
 )
 POL_ZK_PARAMS_PATH = os.environ.get(
     "POL_ZK_PARAMS_PATH",
-    "sigmab/circuit/temp/pol/pol_0001.zkey",
+    "circuit/temp/pol/pol_0001.zkey",
 )
 
 PROVER_PATH = os.environ.get(
     "PROVER_PATH",
-    "sigmab/circuit/rapidsnark/package/bin/prover",
+    "circuit/rapidsnark/package/bin/prover",
 )
 SNARKJS_PATH = os.environ.get("SNARKJS_PATH", ".nvm/versions/node/v20.12.1/bin/snarkjs")
