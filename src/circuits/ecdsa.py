5bcd5170fa0ba5620fd7304a35f064bb4947c486
toolabi <prds.toolabi@gmail.com>
2024-05-21 16:37:58 +0330

    Feature(scripts): changing the signature varibale to seperated r & s in input file.

--- src/circuits/ecdsa.py
+++ src/circuits/ecdsa.py
@@ -2,8 +2,10 @@ from circuits import AbstractCircuit
 from eth_keys import keys
 from eth_utils import keccak, encode_hex
 from ecdsa import VerifyingKey, SECP256k1, util
+from web3 import Web3
+from eth_account.messages import encode_defunct, _hash_eip191_message
 import hashlib
-
+import base64
 
 class ECDSACircuit(AbstractCircuit):
     def _generate_witness(self, msg_hash, r, s, pubkey, salt):
@@ -11,14 +13,17 @@ class ECDSACircuit(AbstractCircuit):
             msghash=msg_hash, r=r, s=s, pubkey=pubkey, salt=salt
         )
 
-    def generate_witness(self, message, public_key, signature, salt):
-        msg_hash = hashlib.sha256(message.encode("utf-8")).digest()
-        msg_hash = int.from_bytes(msg_hash, "big")
-
-        vk = VerifyingKey.from_string(bytes.fromhex(public_key), curve=SECP256k1)
+    def generate_witness(self, message, public_key, signature_r, signature_s, salt):
+        eth_encoded_msg = message.encode("utf-8")
+        message_hash = hashlib.sha256(eth_encoded_msg).digest()
+        message_hash_hex = Web3.to_hex(message_hash)
+        signable = encode_defunct(hexstr=message_hash_hex)
+        hashed = _hash_eip191_message(signable)
+        msg = int.from_bytes(hashed, "big")
+        
+        public_key = base64.b64decode(public_key.encode('utf-8'))
+        vk = VerifyingKey.from_string(public_key, curve=SECP256k1)
         pubkey = vk.pubkey.point
-        curve_order = SECP256k1.order
-        r, s = util.sigdecode_string(signature, curve_order)
 
         def b2a(n, k, x):
             mod = 2**n
@@ -28,10 +33,10 @@ class ECDSACircuit(AbstractCircuit):
                 x = x // mod
             return ret
 
-        r = b2a(64, 4, r)
-        s = b2a(64, 4, s)
-        msg_hash = b2a(64, 4, msg_hash)
+        r = b2a(64, 4, signature_r)
+        s = b2a(64, 4, signature_s)
+        msg_hash = b2a(64, 4, msg)
         pub0 = b2a(64, 4, pubkey.x())
-        pub1 = b2a(64, 4, pubkey.y())
+        pub1 = b2a(64, 4, pubkey.y())   
 
         return self._generate_witness(msg_hash, r, s, [pub0, pub1], salt)
