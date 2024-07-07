d2000f9571f7337d70784aece16d95088f3cd92e
Amirhossein Azarpour <amirh.eth@gmail.com>
2024-07-07 17:48:28 +0330

    feat(extension): update proof example

--- verifier/example/handler.js
+++ verifier/example/handler.js
@@ -1,10 +1,10 @@
 function talk() {
-    console.log()
-    data = {
-        "proofs": PROOF_DATA,
-        "amount": "12000",
-        "uid": "ostadgeorge@gmail.com"
-    };
-    console.log("Triggering SigmaB with data: ", data);
-    window.dispatchEvent(new CustomEvent("TriggerSigmaB", { detail: data }));
-} 
+  console.log();
+  data = {
+    proofs: PROOF_DATA,
+    amount: "700",
+    uid: "09168886497",
+  };
+  console.log("Triggering SigmaB with data: ", data);
+  window.dispatchEvent(new CustomEvent("TriggerSigmaB", { detail: data }));
+}
