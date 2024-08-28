f0457080be73889964cf88e7bf074ff35d1fe01c
toolabi <prds.toolabi@gmail.com>
2024-08-28 15:43:57 +0330

    Fix(core): update example.

--- verifier/example/handler.js
+++ verifier/example/handler.js
@@ -2,8 +2,9 @@ function talk() {
   console.log();
   data = {
     proofs: PROOF_DATA,
-    amount: "1000",
+    amount: "13768760000",
     uid: "blue@gmail.com",
+    liability_salt: "ebbbcd268156dbd845744be48ab6df3118d42e87c1bd36e789ea5944ffd6fdc5"
   };
   console.log("Triggering SigmaB with data: ", data);
   window.dispatchEvent(new CustomEvent("TriggerSigmaB", { detail: data }));
